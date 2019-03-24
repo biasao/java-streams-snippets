c04a1e8f273855f4b834221b231087e3ab955fb2
Alberto Biasao <biasao@gmail.com>
2019-03-24 17:46:45 -0300

    adding examples of filter, map, sort and reduce.

--- streams/src/test/java/blabs/snippets/AppTest.java
+++ streams/src/test/java/blabs/snippets/AppTest.java
@@ -3,8 +3,12 @@ package blabs.snippets;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -39,5 +43,64 @@ public class AppTest {
 		assertTrue("MIGUEL".equals(capitalNames.get(1)));
 		assertTrue("MIRIAN".equals(capitalNames.get(2)));
 		assertTrue("BETO".equals(capitalNames.get(3)));
+	}
+	
+	@Test
+	public void printForEachWithStreams() {
+		IntStream
+			.range(1, 9)
+			.skip(5)
+			.forEach(x -> System.out.println(x));
+	}
+	
+	@Test
+	public void findFirstName() {
+		Optional<String> firstName = Stream.of("Mirian", "Mateus", "Miguel", "Beto")
+									.sorted()
+									.findFirst();
+		
+		assertTrue("Beto".equals(firstName.get()));
+	}
+	
+	@Test
+	public void findAnyNameStartingWithAnM() {
+		Optional<String> anyName = Stream.of("Mirian", "Mateus", "Miguel", "Beto")
+									.findAny();
+		
+		assertTrue("Mirian".equals(anyName.get()));
+	}
+	
+	@Test
+	public void sumOfDoubles() {
+		double total = Stream.of(1.5, 1.5, 1.5)
+								.reduce(0.0, (a,b) -> a + b);
+		
+		assertTrue(total == 4.5);
+	}
+	
+	@Test
+	public void sumOfInts() {
+		final int total = IntStream.range(1, 3)
+									.sum();
+		
+		assertTrue(total == 3);
+	}
+	
+	@Test
+	public void summaryStatsOfInts() {
+		IntSummaryStatistics summary = IntStream.range(0, 100).summaryStatistics();
+		
+		System.out.println(summary);
+	}
+	
+	@Test
+	public void countNamesStartingWithM() {
+		String[] names = {"Beto", "Mi", "Mateus", "Miguel"};
+		
+		final long count = Arrays.stream(names)
+									.filter(n -> n.startsWith("M"))
+									.count();
+		
+		assertTrue(count == 3);
 	}
 }
